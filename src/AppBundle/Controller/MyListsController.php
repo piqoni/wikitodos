42213217b7d5b2af4fa7f57c3024ed3e5992e758
piqoni <piqoni@gmail.com>
2016-09-28 00:05:08 +0200

    myListsController to get users lists

---
+++ src/AppBundle/Controller/MyListsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\UserElements;
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class MyListsController extends Controller
+{
+    /**
+     * @Route("/my", name="myLists")
+     * @Method("POST")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function myAction(Request $request)
+    {
+
+        $user_id = $this->getUser()->getId();
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery('SELECT t.id, t.name 
+                                   FROM AppBundle\Entity\UserElements u 
+                                   JOIN  AppBundle\Entity\Todo t WITH t.id=u.todo_id 
+                                   GROUP BY t.id');
+
+        $myLists = $query->getResult(); 
+
+        $response = new Response(json_encode($myLists));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+}
